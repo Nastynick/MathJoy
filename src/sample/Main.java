aba22c56845e590434e7465f2f94cc8e9f629cb9
Ivan Marosan <37896581+IvanMarosan@users.noreply.github.com>
2019-04-29 10:46:48 +0200

    Test commit
    Hi!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("loginView.fxml"));
-        primaryStage.setTitle("Online shop");
+        primaryStage.setTitle("MathJoy");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
