53699398c25a385a138a78e1023410c193d5213c
Ivan Marosan <37896581+IvanMarosan@users.noreply.github.com>
2019-05-13 11:11:29 +0200

    Changed Main class
    CalculatorView was changed to MenuView

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("MenuView.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("CalculatorView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MenuView.fxml"));
         primaryStage.setTitle("MathJoy");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
