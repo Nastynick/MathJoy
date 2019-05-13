7aaf5d7688ba96b1e37880fb3fec6e637f0e61d8
Ivan Marosan <37896581+IvanMarosan@users.noreply.github.com>
2019-05-13 11:08:48 +0200

    Calculator
    A basic calculator to solve arithmetic functions

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("MenuView.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("CalculatorView.fxml"));
         primaryStage.setTitle("MathJoy");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
