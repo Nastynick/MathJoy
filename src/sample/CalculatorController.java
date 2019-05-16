ca8a1de17e856382d6b3fd10f5bf34cc84013d07
Ivan Marosan <37896581+IvanMarosan@users.noreply.github.com>
2019-05-16 10:21:53 +0200

    Calculator fixes
    Added the ability to store a number into memory, clear memory, clear the calculator and apply negative numbers

--- src/sample/CalculatorController.java
+++ src/sample/CalculatorController.java
@@ -10,28 +10,31 @@ public class CalculatorController {
 
     @FXML
     private Label screenLabel;
+    @FXML
+    private Label memoryLabel;
     private double number1 = 0;
     private double number2 = 0;
+    private double number3 = 0;
     private String operator = "";
     private boolean start = true;
     private Model model = new Model();
 
 
     @FXML
-    public void processNumbers (ActionEvent event) {
+    public void processNumbers(ActionEvent event) {
         if (start) {
             screenLabel.setText("");
             start = false;
         }
-        String value = ((Button)event.getSource()).getText();
-        screenLabel.setText(screenLabel.getText()+ value);
+        String value = ((Button) event.getSource()).getText();
+        screenLabel.setText(screenLabel.getText() + value);
 
     }
 
     @FXML
-    public void procssOperators (ActionEvent event) {
+    public void procssOperators(ActionEvent event) {
         try {
-            String value = ((Button)event.getSource()).getText();
+            String value = ((Button) event.getSource()).getText();
             if (!value.equals("=")) {
                 if (!operator.isEmpty()) {
                     return;
@@ -57,5 +60,39 @@ public class CalculatorController {
         }
     }
 
+    @FXML
+    void processClearMemory (ActionEvent event) {
+        number3 = 0;
+        memoryLabel.setText("Memory: ");
+        screenLabel.setText("");
+    }
+
+    @FXML
+    void processClear(ActionEvent event) {
+        number1 = 0;
+        number2 = 0;
+        number3 = 0;
+        screenLabel.setText("");
+        memoryLabel.setText("Memory: ");
+
+    }
+
+    @FXML
+    void processMemory(ActionEvent event) {
+        try {
+
+            if (number3 == 0) {
+                number3 = Double.parseDouble(screenLabel.getText());
+                screenLabel.setText("");
+                memoryLabel.setText("Memory: " + number3);
+            } else {
+                screenLabel.setText(String.valueOf(number3));
+            }
+
+        } catch (Exception e) {
+            screenLabel.setText("No value can be assigned to memory");
+        }
+
+    }
 }
 
